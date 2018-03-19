012f4b6fe07260eff96364681411cddffe9be3bc
griffin dotson <griffin_dotson@yahoo.com>
2018-03-19 12:31:37 -0700

    Final Project

---
+++ BakeryMVC2018/Controllers/SaleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryMVC2018.Models;
+
+namespace BakeryMVC2018.Controllers
+{
+    public class SaleController : Controller
+    {
+        // GET: Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Purchase()
+        {
+            BakeryEntities db = new BakeryEntities();
+            return View(db.Products.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "email,  phone")]Customer person)
+        {
+           
+
+            return View("Purchase", person);
+
+        }
+    }
+}
---
+++ BakeryMVC2018/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BakeryMVC2018.Models
+{
+    public class Customer
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
