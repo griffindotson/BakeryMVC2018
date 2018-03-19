fad6329ec972f96bc18fd8e3e2d32563aaae5c28
griffin dotson <griffin_dotson@yahoo.com>
2018-03-19 11:41:48 -0700

    Final Project

---
+++ BakeryMVC2018/Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryMVC2018.Models;
+
+namespace BakeryMVC2018.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            BakeryEntities db = new BakeryEntities();
+            return View(db.Products.ToList());
+
+        }
+    }
+}
---
+++ BakeryMVC2018/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryMVC2018.Models;
+
+namespace BakeryMVC2018.Controllers
+{
+    public class RegisterController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "lastName, firstName, email,  phone")]NewPerson person)
+        {
+            Person p = new Person();
+            p.PersonLastName = person.LastName;
+            p.PersonFirstName = person.FirstName;
+            p.PersonEmail = person.Email;
+            p.PersonPhone = person.Phone;
+            p.PersonDateAdded = DateTime.Now;
+
+            db.People.Add(p);
+            db.SaveChanges();
+
+            return View("Confirmation");
+
+        }
+    }
+}
